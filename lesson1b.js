09077e25e1432a84e8c51b42d706803a5473811e
okongoj9-prog <okongoj9@gmail.com>
2026-07-02 03:57:16 -0700

    javascript

---
+++ lesson1b.js
@@ -0,0 +1,27 @@
+// javasript data types
+let name = "joseph kamau"
+console.log("The name of the person is: ", name)
+console.log(typeof(name))
+
+// js Float : It is anumber that has a decimal 
+let myNumber = 87.745
+console.log("The quantity of unga is : ", myNumber)
+
+
+
+// js boolen : thios returns to true /false
+let isAdult = true
+let isRegistered = false
+
+console.log("Is joseph an Adult? ", isAdult)
+console.log("is joseph registed onto our system? ", isRegistered)
+console.log(typeof(isAdult))
+
+// undefined : This is a data type that has been declared but there is no value that has been assigned to it
+let county;
+console.log("Which countyt do you come from? ", county)
+console.log(typeof(county))
+
+// js null : thuis is adata type that contians null values
+let username = null
+console.log("What is your username?", username)
---
+++ lessonia.js
@@ -0,0 +1,25 @@
+// This is a comment...
+/* A variable is a containerthat is used for storing data values */
+// for as to be able declare variable in js, we use  the intilializers i.e var,let and const,
+
+/* The initializer 'let' allows you to declare values that can be changed through out the program*/
+let weight = 56.64
+console.log(weight)
+
+weight = 57.12
+console.log(weight)
+
+// The initializer "const" allows us to declare  variables that cannot be changed i.e they are constants
+const pi = 3.142
+console.log(pi)
+
+
+
+let height = 50
+console.log(height)
+
+let tempurature = 50.6
+console.log(tempurature)
+
+let points = 78
+console.log(points)
