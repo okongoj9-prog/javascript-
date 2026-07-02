2ca6b931b69f77294e68ef69a538f1dbf19e2269
okongoj9-prog <okongoj9@gmail.com>
2026-07-02 04:11:14 -0700

    updates

---
+++ lesson1c.js
@@ -0,0 +1,35 @@
+// js  object Data type
+// An objecrt is adata type that store data in terms of key - Values pairs
+
+let person ={
+    Name : "James Atione",
+    Age : 54,
+    isRegistered : true
+};
+
+console.log("The detials of the person is: ", person)
+
+// To access the items of an object we have 2 methods
+// metod 1 :  by use of squere brackets;
+console.log(person["Name"])
+console.log(person["Age"])
+
+// Method 2 : By use of the dot separator
+console.log(person.isRegistered)
+console.log(person.Name)
+
+// Js Array Data type
+// An Array refers to a collection of items that are related in a certian way and are organised into indexs
+let fruits =["Banana", "Apples", "Mango", "Melon", "Pineapple", "Grapes"]
+console.log(fruits)
+
+
+// To acces the items of an array we use indexs
+console.log(fruits[3])
+console.log(fruits[5])
+
+console.log(fruits,slice[2, 5])
+
+// Research oin js comparison and logical operators .check on conditional
+
+// Research on Js comparison and logical operators. Check on conditional statements and looping structures in Javascript
